0493cab859a115fd7fefa69fe0ae0cae89325e89
simplejack-src <anonymous@anonymous.com>
2019-05-13 12:08:53 -0400

    Split repo, for channel and patch

--- pkgs/pce-n53/default.nix
+++ pkgs/pce-n53/default.nix
@@ -3,21 +3,23 @@
 let
   modDestDir = "$out/lib/modules/${kernel.modDirVersion}/kernel/drivers/net/wireless"; 
   pkg_repo = fetchFromGitHub {
-    repo = "nix-pkgs";
+    repo = "pce-n53";
     owner = "simplejack-src";
-    rev = "v1.1";
-    sha256 = "1js548w27chis3ixzhkx8y78wp5g738jabahsjzwpshyra5ww5hm";
+    rev = "v1.0";
+    sha256 = "1mdybnj2cz9rq6640vpv773l4bhl582jjqkh0dcax5zhnfv8adiv";
   };
 in stdenv.mkDerivation {
   name = "pce-n53-${kernel.version}";
   version = "1.1";
+
+  nativeBuildInputs = [ unzip kernel.moduleBuildDependencies ];
 
   src = fetchurl {
     url = "https://dlcdnets.asus.com/pub/ASUS/wireless/PCE-N53/Linux_PCE_N53_1008.zip";
     sha256 = "c3eb1c4fc5ae8851c079b9c860d2eac9a7db1377bd7e03f253459db435d5d2a3";
   };
 
-  nativeBuildInputs = [ unzip kernel.moduleBuildDependencies ];
+  sourceRoot = ".";
 
   # Unpack the actual source
   postUnpack = ''
@@ -25,7 +27,7 @@ in stdenv.mkDerivation {
   '';
 
   # Patch source so resultant module runs on recent kernels
-  patches = [ "${pkg_repo}/pkgs/pce-n53/fix.patch" ];
+  patches = [ "${pkg_repo}/fix.patch" ];
 
   # Disable troublesome features
   hardeningDisable = [ "format" "fortify" "stackprotector" "pic" ];
